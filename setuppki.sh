d24c28492d8ed7f545d42a12d8720ec0918429ac
jtabbert <101141159+jtabbert@users.noreply.github.com>
2023-06-26 12:14:17 -0400

    Update setuppki.sh

--- setuppki.sh
+++ setuppki.sh
@@ -8,7 +8,7 @@ CONFIG="
   basicConstraints=CA:TRUE,pathlen:0
   "
 
-openssl req -config <(echo "$CONFIG") -new -newkey rsa:2048 -nodes \-subj "/C=IN/O=hashicat/OU=DevSecOps/ST=AP/CN=hashicat.com/emailAddress=hashicat@hashicorp.com" -x509 -days 365 -extensions ext -keyout root-key.pem -out root-cert.pem
+openssl req -config <(echo "$CONFIG") -new -newkey rsa:2048 -nodes \-subj "/C=IN/O=hashicups/OU=Cloud-DevOps/ST=AP/CN=hashicups.com/emailAddress=hashicups@hashicorp.com" -x509 -days 365 -extensions ext -keyout root-key.pem -out root-cert.pem
 
 cat root-key.pem root-cert.pem > pem_bundle
 
@@ -18,25 +18,25 @@ vault secrets tune -max-lease-ttl=8760h pki
 
 vault write pki/config/ca pem_bundle=@pem_bundle ttl=8760h
 
-vault write pki/roles/hashicat allowed_domains=hashicat.com allow_subdomains=true max_ttl=3m
+vault write pki/roles/hashicups allowed_domains=hashicups.com allow_subdomains=true max_ttl=3m
 
 vault write pki/config/urls issuing_certificates="$VAULT_ADDR/v1/pki/ca" crl_distribution_points="$VAULT_ADDR/v1/pki/crl"
 
-vault policy write hashicat_pki - << EOF
+vault policy write hashicups_pki - << EOF
 # policies required for PKI demo
 path "pki*" {
   capabilities = ["read", "list"]
 }
 
-path "pki/roles/hashicat" {
+path "pki/roles/hashicups" {
   capabilities = ["create", "update"]
 }
 
-path "pki/sign/hashicat" {
+path "pki/sign/hashicups" {
   capabilities = ["create", "update"]
 }
 
-path "pki/issue/hashicat" {
+path "pki/issue/hashicups" {
   capabilities = ["create", "update", "read", "list"]
 }
 EOF
@@ -51,7 +51,7 @@ EOF
 vault write auth/kubernetes/role/issuer \
 bound_service_account_names=issuer \
 bound_service_account_namespaces=default \
-policies=hashicat_pki \
+policies=hashicups_pki \
 ttl=20m
 
 echo 'apiVersion: v1
@@ -60,14 +60,14 @@ metadata:
   annotations:
     vault.hashicorp.com/agent-inject: "true"
     vault.hashicorp.com/agent-init-first: "true"
-    vault.hashicorp.com/agent-inject-secret-server.key: "pki/issue/hashicat"
+    vault.hashicorp.com/agent-inject-secret-server.key: "pki/issue/hashicups"
     vault.hashicorp.com/agent-inject-template-server.key: |
-      {{- with secret "pki/issue/hashicat" "common_name=test.hashicat.com" -}}
+      {{- with secret "pki/issue/hashicups" "common_name=test.hashicups.com" -}}
       {{ .Data.private_key }}
       {{- end }}
-    vault.hashicorp.com/agent-inject-secret-server.crt: "pki/issue/hashicat"
+    vault.hashicorp.com/agent-inject-secret-server.crt: "pki/issue/hashicups"
     vault.hashicorp.com/agent-inject-template-server.crt: |
-      {{- with secret "pki/issue/hashicat" "common_name=test.hashicat.com" -}}
+      {{- with secret "pki/issue/hashicups" "common_name=test.hashicups.com" -}}
       {{ .Data.certificate }}
       {{- end }}
     vault.hashicorp.com/role: "issuer"
